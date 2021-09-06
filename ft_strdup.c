c24884dd3ba93a530e331b1d2a203d6ddacae524
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-06 16:27:44 +0930

    Complete ft_strdup.c, ft_strlcat.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,3 +1,10 @@
 #include "libft.h"
 
+char	*ft_strdup(const char *s1)
+{
+	char	*dup;
 
+	dup = (char *) malloc(sizeof(src) + 1);
+	ft_strcpy(dup, src);
+	return (dup);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+size_t	strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] && i < size)
+		i++;
+	while (src[j] && (i + j + 1) < size)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	if (i < size)
+		dest[i + j] = '\0';
+	return (i + ft_strlen(src));
+}
