e16d593dd8cb1b7ef60d1d1374796952f80bb47f
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-06 14:26:55 +0930

    Complete ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t idx;
+
+	idx = 0;
+	while (idx < len)
+    {
+        ((unsigned char *)b)[idx] = c;
+        idx++;
+    }
+	return (b);
+}
