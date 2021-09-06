cc4799d9c5b5b0a67d388210fa4677ff87407c95
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-06 16:30:23 +0930

    Complete ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while (n > 0 && *s1 && *s2)
+	{
+		if (*s1 != *s2)
+			break ;
+		s1++;
+		s2++;
+		n--;
+	}
+	if (n == 0)
+		return (0);
+	return (*(unsigned char *) s1 - *(unsigned char *) s2);
+}
