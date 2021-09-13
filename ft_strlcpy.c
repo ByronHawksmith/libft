59b531779b85c4990171bec836fa0a45ae819c7c
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-13 12:27:39 +0930

    Some changes to ft_strlen.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -2,16 +2,14 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	const size_t	srclen = ft_strlen(src);
+	const size_t	src_len = ft_strlen(src);
 
-	if (srclen + 1 < size)
-	{
-		ft_memcpy(dst, src, srclen + 1);
-	}
+	if (src_len + 1 < size)
+		ft_memcpy(dst, src, src_len + 1);
 	else if (size != 0)
 	{
 		ft_memcpy(dst, src, size - 1);
 		dst[size - 1] = '\0';
 	}
-	return (srclen);
+	return (src_len);
 }
