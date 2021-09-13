6af628483b15121fd77e0b0a11d73b169e1c4b47
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-13 11:47:24 +0930

    Remote malloc from ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,15 +2,24 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*tmp;
+	char		*d;
+	char		*last_d;
+	const char	*s;
+	const char	*last_s;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
-	tmp = (char *) malloc(sizeof(char) * len);
-	if (tmp == NULL)
-		return (NULL);
-	ft_memcpy(tmp, src, len);
-	ft_memcpy(dst, tmp, len);
-	free(tmp);
+	d = dst;
+	s = src;
+	if (d < s)
+		while (len--)
+			*d++ = *s++;
+	else
+	{
+		last_s = s + (len - 1);
+		last_d = d + (len - 1);
+		while (len--)
+			*last_d-- = *last_s--;
+	}
 	return (dst);
 }
