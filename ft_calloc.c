d26f4a5522408378e9b6bd92e67ef884394e33b4
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-13 16:24:40 +0930

    Implement ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void *arr;
+	size_t req;
+
+	req = count * size;
+	CHECK_MALLOC(arr, req);
+	ft_memset(arr, 0, req);
+	return (arr);
+}
