fbd42be7f07b6e32bff88d95750ae76a341bd4c2
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-13 14:29:47 +0930

    Fix ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,19 +1,18 @@
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	j;
+	size_t	needle_len;
 
-	i = 0;
-	while (i < n && s1[i])
+	needle_len = ft_strlen(needle);
+	if (!needle_len)
+		return ((char *) haystack);
+	while (len >= needle_len)
 	{
-		j = 0;
-		while (i + j < n && s1[i + j] == s2[j])
-			j++;
-		if (s2[j])
-			return ((char *) s1 + i);
-		i++;
+		len--;
+		if (!ft_memcmp(haystack, needle, needle_len))
+			return ((char *) haystack);
+		haystack++;
 	}
 	return (NULL);
 }
