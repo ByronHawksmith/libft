1c677802bee3326618f7f116d95b460541174a4d
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-13 16:08:33 +0930

    Fix ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,15 +5,29 @@ static int	ft_isspace(int c)
 	return ((c >= 9 && c <= 13) || (c == 32));
 }
 
+static int	ft_long_border(const char c, int mult, unsigned long nb)
+{
+	unsigned long	border;
+
+	border = 922337203685477580;
+	if ((nb > border || (nb == border && (c - '0') > 7)) && mult == 1)
+		return (-1);
+	else if ((nb > border || (nb == border && (c - '0') > 8)) && mult == -1)
+		return (0);
+	return (1);
+}
+
 int	ft_atoi(const char *str)
 {
-	int i;
-	int mult;
-	int nb;
+	int				i;
+	int				mult;
+	unsigned long	nb;
+	int				brd;
 
 	i = 0;
 	mult = 1;
 	nb = 0;
+	brd = 1;
 	while (ft_isspace(str[i]))
 		i++;
 	while (str[i] == '-' || str[i] == '+')
@@ -24,6 +38,9 @@ int	ft_atoi(const char *str)
 	}
 	while (ft_isdigit(str[i]))
 	{
+		brd = ft_long_border(str[i], mult, nb);
+		if (brd < 1)
+			return (brd);
 		nb *= 10;
 		nb += str[i] - '0';
 		i++;
