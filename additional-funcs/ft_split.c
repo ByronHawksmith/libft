caad26f026cb35ed9e8142856f091f9c3643721e
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-20 19:50:35 +0930

    Remove ft_count_strings.c

--- additional-funcs/ft_split.c
+++ additional-funcs/ft_split.c
@@ -6,11 +6,30 @@
 /*   By: bhawksmi <bhawksmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 14:02:28 by bhawksmi          #+#    #+#             */
-/*   Updated: 2021/09/20 19:20:12 by bhawksmi         ###   ########.fr       */
+/*   Updated: 2021/09/20 19:49:48 by bhawksmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	ft_count_strings(char const *str, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] != c && str[i] != '\0')
+			count++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
+	}
+	return (count);
+}
 
 static size_t	ft_strlendel(char const *s, char c)
 {
