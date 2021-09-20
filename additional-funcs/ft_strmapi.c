81df96508671a78a4fd9a22df2bb4207f9451c60
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-20 14:24:54 +0930

    Complete ft_strmapi.c

--- additional-funcs/ft_strmapi.c
+++ additional-funcs/ft_strmapi.c
@@ -6,8 +6,22 @@
 /*   By: bhawksmi <bhawksmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 14:02:29 by bhawksmi          #+#    #+#             */
-/*   Updated: 2021/09/16 14:02:29 by bhawksmi         ###   ########.fr       */
+/*   Updated: 2021/09/20 13:58:44 by bhawksmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new_str;
+	int		i;
+
+	CHECK_PTR(s);
+	new_str = ft_strnew(ft_strlen(s));
+	CHECK_PTR(new_str);
+	i = -1;
+	while (*(s + ++i))
+		*(new_str + i) = f(i, *(s + i));
+	return (new_str);
+}
