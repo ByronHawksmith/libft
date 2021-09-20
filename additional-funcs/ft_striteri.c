d57e4dc994aa41a828bd3c7a488db046310c5a73
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-20 14:38:51 +0930

    Complete ft_striteri.c

--- additional-funcs/ft_striteri.c
+++ additional-funcs/ft_striteri.c
@@ -6,8 +6,16 @@
 /*   By: bhawksmi <bhawksmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 14:02:28 by bhawksmi          #+#    #+#             */
-/*   Updated: 2021/09/16 14:02:28 by bhawksmi         ###   ########.fr       */
+/*   Updated: 2021/09/20 14:38:34 by bhawksmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
 
+	i = -1;
+	if (s && f)
+		while (*(s + ++i))
+			f(i, s + i);
+}
