b02d76ca273bf79504bdab92c5bbb58ef62f30b9
Byron Hawksmith <60097760+ByronHawksmith@users.noreply.github.com>
2021-09-22 17:31:44 +0930

    Fix accidental angle bracket include.

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 static char	*ft_strnew(size_t size)
 {
