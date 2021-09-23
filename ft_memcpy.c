8b9b93b7873a486a20f7212ae474fd5769dacdcb
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-09-23 20:08:08 +0930

    Fix ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: bhawksmi <bhawksmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 17:27:44 by bhawksmi          #+#    #+#             */
-/*   Updated: 2021/09/22 18:56:24 by bhawksmi         ###   ########.fr       */
+/*   Updated: 2021/09/23 19:52:21 by bhawksmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char		*d;
-	const char	*s;
+	unsigned char	*d;
+	unsigned char	*s;
 
 	if (!dst || !src)
-		return (NULL);
-	d = dst;
-	s = src;
+		return (dst);
+	d = (unsigned char *) dst;
+	s = (unsigned char *) src;
 	while (n)
 	{
 		*d = *s;
