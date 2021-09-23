2f7ea03b2d9f4af5df619454ce464594908ca42f
Byron Hawksmith <byronhawksmith@protonmail.com>
2021-09-23 22:36:45 +0930

    Change types in ft_memcpy.c again

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,13 +14,13 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	char		*d;
+	const char	*s;
 
 	if (!dst || !src)
 		return (dst);
-	d = (unsigned char *) dst;
-	s = (unsigned char *) src;
+	d = dst;
+	s = src;
 	while (n)
 	{
 		*d = *s;
