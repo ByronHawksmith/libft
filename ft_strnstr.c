daa8f9da98bddcfb6ad49c3fad1cdd9e6083029f
Byron Hawksmith <byronhawksmith@protonmail.com>
2021-09-23 23:51:16 +0930

    Fix memory overbounding in ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -19,7 +19,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	needle_len = ft_strlen(needle);
 	if (!needle_len)
 		return ((char *) haystack);
-	while (len >= needle_len)
+	while (len >= needle_len && *haystack)
 	{
 		len--;
 		if (!ft_memcmp(haystack, needle, needle_len))
